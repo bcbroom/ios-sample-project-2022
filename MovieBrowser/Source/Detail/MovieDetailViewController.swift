dcc0501adf1bd3a20e0ca645b8b646def9db4ec8
Brian Broom <brian.broom@gmail.com>
2022-03-11 13:51:35 -0500

    Add formatter for release date in Detail screen

--- MovieBrowser/Source/Detail/MovieDetailViewController.swift
+++ MovieBrowser/Source/Detail/MovieDetailViewController.swift
@@ -16,6 +16,8 @@ class MovieDetailViewController: UIViewController {
   @IBOutlet var descriptionTextView: UITextView!
   
   private var selectedMovie: Movie
+  private let inputDateFormatter = DateFormatter()
+  private let outputDateFormatter = DateFormatter()
   
   // not called, but compiler complains if it's missing
   required init?(coder: NSCoder) {
@@ -25,12 +27,28 @@ class MovieDetailViewController: UIViewController {
   init?(movie: Movie, coder: NSCoder) {
     self.selectedMovie = movie
     super.init(coder: coder)
+    
+    inputDateFormatter.locale = Locale(identifier: "en_US_POSIX")
+    inputDateFormatter.dateFormat = "yyyy-MM-dd"
+    
+    outputDateFormatter.locale = Locale.current
+    outputDateFormatter.dateStyle = .long
   }
   
   override func viewDidLoad() {
     titleLabel.text = selectedMovie.title
     releaseDateLabel.text = selectedMovie.releaseDate
     descriptionTextView.text = selectedMovie.overview
+    
+    // duplicated from Cell logic, but I'm not sure where it should end up
+    // the parsing should probably live in the Network class, but that's setup with
+    // static methods, so no good place for the formatters to live.
+    if let releaseDate = selectedMovie.releaseDate,
+       let parsedDate = inputDateFormatter.date(from: releaseDate) {
+      releaseDateLabel.text = "Release Date: \(outputDateFormatter.string(from: parsedDate))"
+    } else {
+      releaseDateLabel.text = "Release Date not available"
+    }
     
     if let posterURL = selectedMovie.posterPath {
       Network.fetchPosterImage(from: posterURL) { image in
